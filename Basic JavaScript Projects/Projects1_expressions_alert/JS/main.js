d354f172e151f7906781458c0ad880c93dfccdfd
jamzt <jamietaalai@gmail.com>
2023-09-23 12:11:10 -0700

    main.js and index assignment

---
+++ Basic JavaScript Projects/Projects1_expressions_alert/JS/main.js
@@ -0,0 +1,34 @@
+window.alert("Hello, World! This code is in an external file ^~^");  //calling function window.alert
+
+var A = "This is an assigned to variable string."; //assigning varaible A  - a value 
+document.write(A);  // calling doc write function
+
+var cat = "\"Meow...\" said Neo. He wasn\'t hungry he just needed cuddles."
+document.write(cat);
+
+
+document.write("\"Be who you are and say what you fell," + " because those who mind don\'t matter and those who matter don\'t mind.\"" + "- Dr. Seuss");
+var X = "\"Everything was beautiful and nothing hurt." + "- Kurt Vonnegut\""
+document.write(X)
+
+var Family = "The Arezziniz", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";
+document.write(Dad);
+
+var state = "Washington", town = "Duvall", city = "Chicago", beach= "Pescadero";
+document.write(beach)
+
+document.write(2+2)
+
+
+function My_First_Function() {  //calling fucntion my first function
+    var str = "This is the button text!"; // defining var str as "buttton text"
+    document.getElementById("Button_Text").innerHTML = str; // using get element by id method to get specific element
+}
+
+var Sent1 ="This os the beginning of the string"
+var Sent2 ="and this is the end of the string."
+document.write(Sent1 + Sent2)
+
+
+document.write(66 - 33)
+
