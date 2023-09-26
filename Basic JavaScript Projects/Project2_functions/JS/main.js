1fc1f0ac11994528d938b56d9d75fd1d1d92db1e
jamzt <jamietaalai@gmail.com>
2023-09-25 20:04:25 -0700

    pushing += files

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function My_Function() {
+    var island1 = "Greenland";
+    var island2 = "Madagascar"; // defining var str as "buttton text"
+    document.getElementById("Answer_Button").innerHTML = island1; // using get element by id method to get specific element
+}
+
---
+++ Basic JavaScript Projects/Project2_functions/JS/mainplus.js
@@ -0,0 +1,5 @@
+function myFunction() { //created function named myFunction
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence; //concatenating 2 strings with += 
+}
